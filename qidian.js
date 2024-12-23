6ba29f64cf3ce0bd5c28c7e92c135a8cf549b488
intsingZ <ztd114@outlook.com>
2024-12-24 02:45:15 +0800

    Update qidian.js

--- qidian.js
+++ qidian.js
@@ -7,7 +7,7 @@
 🎯重写脚本:
 [rewrite local]
 https\:\/\/h5\.if\.qidian\.com\/argus\/api\/v1\/video\/adv\/finishWatch url script-request-body https://raw.githubusercontent.com/MCdasheng/QuantumultX/main/Scripts/myScripts/qidian/qidian.cookie.js
-https\:\/\/h5\.if\.qidian\.com\/argus\/api\/v1\/video\/adv\/mainPage url script-response-body https://raw.githubusercontent.com/MCdasheng/QuantumultX/main/Scripts/myScripts/qidian/qidian.taskId.js
+https\:\/\/h5\.if\.qidian\.com\/argus\/api\/v2\/video\/adv\/mainPage url script-response-body https://raw.githubusercontent.com/MCdasheng/QuantumultX/main/Scripts/myScripts/qidian/qidian.taskId.js
 [MITM]
 hostname = h5.if.qidian.com
 ⏰定时任务:
