c4f0d27dbfb1d87144cdedcf315796986919ced6
intsingZ <ztd114@outlook.com>
2024-12-24 08:41:14 +0800

    Update qd_task.js

--- qd_task.js
+++ qd_task.js
@@ -3,7 +3,7 @@
 操作步骤: 我 --> 福利中心 
 
 [rewrite local]
-https\:\/\/h5\.if\.qidian\.com\/argus\/api\/v2\/video\/adv\/mainPage url script-response-body https://raw.githubusercontent.com/intsingZ/Quantumultx/refs/heads/main/qd_task.js
+https\:\/\/h5\.if\.qidian\.com\/argus\/api\/v1\/video\/adv\/mainPage url script-response-body https://raw.githubusercontent.com/intsingZ/Quantumultx/refs/heads/main/qd_task.js
 
 [MITM]
 hostname = h5.if.qidian.com
